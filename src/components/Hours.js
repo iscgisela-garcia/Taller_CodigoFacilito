0946520da32d6dd371fc1f852d0b38fce6035b57
iscgisela-garcia <isc.giselagarcia@gmail.com>
2024-06-12 21:43:51 +0000

    mensaje

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,18 @@
-// Placeholder to host a component
+import React from 'react';
+ 
+const Hours = () => {
+  const hours = Array.from({ length: 24 }, (_, i) => i);
+ 
+  return (
+<div>
+<h2>Horas del día</h2>
+<ul>
+        {hours.map((hour) => (
+<li key={hour}>{hour}:00</li>
+        ))}
+</ul>
+</div>
+  );
+};
+ 
+export default Hours;// Placeholder to host a component
